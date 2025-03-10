7616f6429c4bfc3413e41baf6bf59c53b740b49a
jchabert0 <jchabert0@gmail.com>
2025-03-10 13:58:29 +0100

    css

--- src/functions/getProducts.ts
+++ src/functions/getProducts.ts
@@ -85,7 +85,9 @@ return `
 <div class="order-content__title">
 <h3>${order.title}</h3>
 <h4>${order.price}€</h4>
-      <button class="secondary deleteOrder" data-id="${order.id}">delete to cart</button>
+      <button class="deleteOrder" data-id="${order.id}">
+      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="var(--error-color)"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
+      </button>
 </div>
 </div>
 `
